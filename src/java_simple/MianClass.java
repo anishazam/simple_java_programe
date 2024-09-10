5601c178b5ebc74a0fb3eef4eb960af6bc95a2d5
anishazam <anishazam.mca@gmail.com>
2024-09-11 00:50:20 +0530

    add one more console

--- src/java_simple/MianClass.java
+++ src/java_simple/MianClass.java
@@ -4,6 +4,7 @@ public class MianClass {
 	
 	public static void main(String[] args) {
 		System.out.println("hello, this is simple programe of java");
+		System.out.println("hello, my name is anish azam");
 	}
 
 }
